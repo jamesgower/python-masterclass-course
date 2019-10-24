cc50ed506ba830c6eca41dea1eefe648984fd4af
James <james_gower@outlook.com>
2019-10-24 12:04:58 +0100

    sorted files into folders, added new modules

--- lists/lists.py
+++ lists/lists.py
@@ -4,7 +4,7 @@ for state in parrot_list:
     print("This parrot is " + state)
 
 even = [2, 4, 6, 8]
-odd = [1,3,5,7,9]
+odd = [1, 3, 5, 7, 9]
 nums = even + odd
 numbers_in_order = sorted(nums)
 print(nums)
@@ -20,4 +20,5 @@ another_even = list(even)
 print(another_even is even)
 
 another_even.sort(reverse=True)
+even  #?
 print(even)
---
+++ sets/sets.py
@@ -0,0 +1,81 @@
+farm_animals = {"sheep", "cow", "hen"};
+print(farm_animals)
+
+for animal in farm_animals:
+    print(animal)
+
+print("=" * 40)
+
+wild_animals = set(["lion", "tiger", "panther", "elephant", "hare"])
+print(wild_animals)
+
+for animal in wild_animals:
+    print(animal)
+
+farm_animals.add("horse")
+wild_animals.add("horse")
+
+print(farm_animals, wild_animals)
+
+empty_set = set()
+empty_set_fail = {}
+# will fail as it becomes a dictionary, not set - which doesnt have add method
+# empty_set_fail.add()
+
+even = set(range(0, 40, 2))
+print(even)
+print(len(even))
+
+squares_tuple = (4, 6, 9, 16, 25)
+squares = set(squares_tuple)
+print(squares)
+print(len(squares))
+
+# union method merges two sets into one
+print(even.union(squares))
+print(len(even.union(squares)))
+
+# intersection method returns the values of two sets where contain the same value
+# it can be called with the "intersection" method or by calling "&"
+print("-" * 40)
+print(even.intersection((squares)))
+print(even & squares)
+print(squares.intersection((even)))
+print(squares & even)
+
+print("_" * 40)
+
+# difference method takes two arguments (a and b). The set difference of a and b is a set of elements
+# that exists only in set a but not set b. It can be invoked by the difference method or by using - between
+# two sets.
+print(sorted(even))
+print(sorted(squares))
+
+print("even minus squares")
+print(sorted(even.difference(squares)))
+print(sorted(even - squares))
+
+print("squares minus even")
+print(sorted(squares.difference(even)))
+print(sorted(squares - even))
+
+
+# the difference_update method changes the first set to be the difference between itself and the second list,
+# i.e it removes all elements from set b from set a, and stores the value in set a.
+print("=" * 40)
+print(sorted(even))
+print(squares)
+even.difference_update(squares)
+print(sorted(even))
+
+print("=" * 40)
+
+even = set(range(0, 40, 2))
+print(sorted(even))
+print(sorted(squares))
+
+# symmetric difference returns a set which contains values which are either in set a or set b, but not both:
+# the method can be called by using the method or by using a caret (^) between two sets.
+print("symmetric even minus squares")
+print(sorted(even.symmetric_difference(squares)))
+print(sorted(even ^ squares))
